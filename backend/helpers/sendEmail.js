4923cb6ba0c15682673df96860c0d66442626fe4
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-01-03 00:11:36 +0530

    Changed email sending script

--- backend/helpers/sendEmail.js
+++ backend/helpers/sendEmail.js
@@ -1,28 +1,43 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer")
+const { google } = require("googleapis")
 
-// Transporter object
-const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false,
-    tls: {
-        rejectUnauthorized: false
-    },
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    },
-})
+// Authenticate
+const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
 
 // Email sending
 exports.sendEmail = async (email, regNum, subject, password) => {
     try {
+        // Generate access token
+        const accessToken = await oAuth2Client.getAccessToken()
+        // console.log(accessToken)
+
+        // Transporter object
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            secure: false,
+            port: 25,
+            auth: {
+                type: "OAuth2",
+                user: process.env.EMAIL,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: accessToken
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+
+        // Send mail
         await transporter.sendMail({
             from: `Izoid Education Center ${process.env.EMAIL}`,
             to: email,
             subject: subject,
-            html: `<h2>${subject}</h2><br><h3>Here is your credentials...</h3><br><b>Registration Number : ${regNum}</b><br><b>Password : ${password}</b>`
-        });
+            text: `Here is your credentials, Registration Code : ${regNum} | Password : ${password}`,
+            html: `<h2>${subject}</h2><br><h3>Here is your credentials...</h3><br><b>Registration Code : ${regNum}</b><br><b>Password : ${password}</b>`
+        })
     } catch (err) {
         console.log(err.message)
     }
